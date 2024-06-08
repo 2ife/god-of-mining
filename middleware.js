fc58c7c12946c0d6112db779d0f9cd66c7b40046
2ife <2ife1601@gmail.com>
2024-06-08 17:16:54 +0900

    before start 13krfds

--- middleware.js
+++ middleware.js
@@ -23,6 +23,7 @@ const apiLimiter = (0, express_rate_limit_1.rateLimit)({
         xForwardedFor: req.headers["x-forwarded-for"],
       }})`
     );
+    
     res.send("단기간 내 너무 많은 데이터를 요청하여 1분 이후에 접속 바랍니다.");
   },
 });
