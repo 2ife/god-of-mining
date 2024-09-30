c4ab839f0df5f53bee9a5c73ee9b3f305d7bc2af
2ife <2ife1601@gmail.com>
2024-09-30 14:39:13 +0900

    error fix

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,8 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+// var __importDefault = (this && this.__importDefault) || function (mod) {
+//     return (mod && mod.__esModule) ? mod : { "default": mod };
+// };
+// Object.defineProperty(exports, "__esModule", { value: true });
 // const axios = __importDefault(require("axios"));
 // html
 const loginContainer = document.querySelector("#loginContainer");
